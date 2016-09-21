9d17c9c949bf6551bfa3a13f93a3bcacf35d400a
Sergio-Feliciano Mendoza-Barrera <smendoza.barrera@gmail.com>
2016-09-20 20:07:29 -0500

    Contact form test :: v0.0.24

--- email.php
+++ email.php
@@ -2,36 +2,32 @@
 
 //-----------------------------------------------------
 //-----------change address to xxx@yourdomainname ----------------------
-// $address= "contact@naiknikunj.me";
-$address= "s.f.m@ieee.org";
+// $address= 'contact@naiknikunj.me';
+$address= 's.f.m@ieee.org';
 //-----------------------------------------------------
 //-----------------------------------------------------
 
-$name = $_POST["name"];
-print "name is $name";
+$name = $_POST['name'];
+$startup = $_POST['startup'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$subject = 'New Email';
+$message_content = $_POST['comments'];
 
-$startup = $_POST["startup"];
-print "name is $startup";
+$headers = 'From: $name <$email>\r\n';
+$headers .= 'Reply-To: $subject <$email>\r\n';
 
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$subject = "New Email";
-$message_content = $_POST["comments"];
+$message = '--$mime_boundary \r\n';
 
-$headers = "From: $name <$email>\r\n";
-$headers .= "Reply-To: $subject <$email>\r\n";
-
-$message = "--$mime_boundary \r\n";
-
-$message .= "You have an email from your web site: \r\n";
-$message .= "Name: $name \r\n";
-$message .= "Startup: $startup \r\n";
-$message .= "Email: $email \r\n";
-$message .= "phone: $phone \r\n";
-$message .= "Message: $message_content \r\n";
-$message .= "--$mime_boundary--\r\n";
+$message .= 'You have an email from your web site: \r\n';
+$message .= 'Name: $name \r\n';
+$message .= 'Startup: $startup \r\n';
+$message .= 'Email: $email \r\n';
+$message .= 'phone: $phone \r\n';
+$message .= 'Message: $message_content \r\n';
+$message .= '--$mime_boundary--\r\n';
 $mail_sent = mail($address, $subject, $message, $headers);
 if($mail_sent)
 {
-    echo "success";
+    echo 'success';
 }
