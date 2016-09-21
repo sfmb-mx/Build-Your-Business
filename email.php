115657cca28fbc699de3b85030c6259e81fbfeee
Sergio-Feliciano Mendoza-Barrera <smendoza.barrera@gmail.com>
2016-09-20 19:20:12 -0500

    Contact form test :: v0.0.20

--- email.php
+++ email.php
@@ -1,32 +1,34 @@
 <?php
 
-    //-----------------------------------------------------
-    //-----------change address to xxx@yourdomainname ----------------------
-/* $address= "contact@naiknikunj.me";*/
+//-----------------------------------------------------
+//-----------change address to xxx@yourdomainname ----------------------
+// $address= "contact@naiknikunj.me";
 $address= "s.f.m@ieee.org";
-    //-----------------------------------------------------
-    //-----------------------------------------------------
+//-----------------------------------------------------
+//-----------------------------------------------------
 
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $subject = "New Email";
-    $message_content = $_POST["comments"];
+$name = $_POST["name"];
+/* $email = $_POST["email"];*/
+$email = "jorge@build-yourbusiness.com";
+$startup = _POST["startup"];
 
+$phone = $_POST["phone"];
+$subject = "Customer contact";
+$message_content = $_POST["comments"];
 
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $subject <$email>\r\n";
+$headers = "From: $name <$email>\r\n";
+$headers .= "Reply-To: $subject <$email>\r\n";
 
-    $message = "--$mime_boundary \r\n";
+$message = "--$mime_boundary \r\n";
 
-    $message .= "You have an email from your web site: \r\n";
-    $message .= "Name: $name \r\n";
-    $message .= "Email: $email \r\n";
-    $message .= "phone: $phone \r\n";
-    $message .= "Message: $message_content \r\n";
-    $message .= "--$mime_boundary--\r\n";
-    $mail_sent = mail($address, $subject, $message, $headers);
-    if($mail_sent)
-    {
-        echo "success";
-    }
+$message .= "You have an email from your web site: \r\n";
+$message .= "Name: $name \r\n";
+$message .= "Startup: $startup \r\n";
+$message .= "phone: $phone \r\n";
+$message .= "Message: $message_content \r\n";
+$message .= "--$mime_boundary--\r\n";
+$mail_sent = mail($address, $subject, $message, $headers);
+if($mail_sent)
+{
+    echo "success";
+}
